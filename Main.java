2b1f8dedd2e26326fa47cffc3454bc821e7cec8b
DanNdu7 <danndu7@gmail.com>
2016-02-10 18:08:54 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
-public class Main{
-	public static void main(String[] args){
-		
-	}
+import java.util.ArrayList;
+import java.util.Random;
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    // Creer un joueur
+    Player player = new Player("Daniel", "German");
+    
+    // Initialiser les donnees
+   	player.setInfo(player);
+    
+    // Imprimer le nom et empire du joueur
+    System.out.println(player);
+    
+    // Imprimer ces information
+    System.out.println(player.getInfo(player));
+    
+    // Faire une attaque
+    String troop = "Milice";
+    player.Attack(troop);
+  }
 }
